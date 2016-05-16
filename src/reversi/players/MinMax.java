4d199203fbf8d341ad396a705285cbfd7c3f6b4e
Samuel Jeune <sam-j@hotmail.fr>
2016-05-16 15:10:18 +0200

    amelioration de la fonction d'eval avec un tableau complet des valeurs des cases

--- src/reversi/players/MinMax.java
+++ src/reversi/players/MinMax.java
@@ -19,9 +19,31 @@ import reversi.Player;
  */
 public class MinMax extends Player {
 
+    
+    
     public MinMax(int numPlayer) {
         super(numPlayer);
     }
+ 
+    
+    
+    //tableau des valeurs des positions
+    private final int[][] tabEval = {{8,-2,3,3,3,3,-2,8},{-2,1,2,2,2,2,1,-2},{3, 2, 1, 1, 1, 1, 2, 3},{3, 2, 1, 1, 1, 1, 2, 3},{3, 2, 1, 1, 1, 1, 2, 3},{3, 2, 1, 1, 1, 1, 2, 3},{-2, 1, 2, 2, 2, 2, 1,-2},{8,-2, 3, 3, 3, 3,-2, 8}};
+
+
+    
+    //fonction d'évaluation
+    public int eval(int x, int y, int player, Board board) {
+
+        int eval = 0;
+        int pos = 0;        
+        pos = tabEval[x][y]; 
+        eval=200*pos;
+        return eval;
+    }
+    
+    
+    
     
     public Move minMaxPlay (Game game)
         {
@@ -33,17 +55,18 @@ public class MinMax extends Player {
                 return null;
             }
             
+            //initialisation du bestMov sur une valeurs random
             Random randomGenerator = new Random();
             int randomInt = randomGenerator.nextInt(possibilities.size());                
             Move bestMov = new Move(possibilities.get(randomInt).getX(),possibilities.get(randomInt).getY());
             
-                         
+            //pour chaque possibilité, test des possiblités de l'adversaire et bestMov devient celle qui rapporte le moins de points a l'adversaire             
             for (int k=0; k<possibilities.size(); k++){
                 Board subBoard = game.getBoard().clone();
                 game.mouvementAction(bestMov, this, subBoard);
                 Vector<Move>posAdverse = game.getPlayer(this.getOpponent()).possibilities(game);
                 for (int l=0; l<posAdverse.size(); l++){
-                    eval = game.eval(posAdverse.get(l).getX(),posAdverse.get(l).getY(), this.getOpponent(), subBoard);
+                    eval = eval(posAdverse.get(l).getX(),posAdverse.get(l).getY(), this.getOpponent(), subBoard);
                 
                 if (score>eval) {
                     score = eval ;
